42feda3b616a64ff33a4f45f81248f420625efb4
papajefe <jjaldridge2009@gmail.com>
2025-04-16 17:50:13 -0400

    Implement DS Frame Rate into frames_to_ms
    Make ms recommendations more accurate by accounting for frame rate differences by system.

--- js_finder/js_finder/util/__init__.py
+++ js_finder/js_finder/util/__init__.py
@@ -1,4 +1,13 @@
 from math import floor
 
-def frame_to_ms(frame):
-    return floor((frame) / (16777216 / 280896) * 1000)
+system_data = {
+    "RSE": {"frame_rate": (16777216 / 280896), "offset_ms": 0},   
+    "GBA": {"frame_rate": (16777216 / 280896), "offset_ms": -260},
+    "GBP": {"frame_rate": (16777216 / 280896), "offset_ms": 200},
+    "NDS": {"frame_rate": (16756991 / 280896), "offset_ms": 788},
+    "3DS": {"frame_rate": (16756991 / 280896), "offset_ms": 1558},
+}
+
+def frame_to_ms(frame, system):
+        return floor((frame) / (system_data[system]["frame_rate"]) * 1000) + system_data[system]["offset_ms"]
+
