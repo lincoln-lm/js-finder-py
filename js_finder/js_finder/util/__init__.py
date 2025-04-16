338bd0dd24fbe83d8fbf36beb475411196d8d94a
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-04-16 16:17:25 -0600

    clean frame_to_ms

--- js_finder/js_finder/util/__init__.py
+++ js_finder/js_finder/util/__init__.py
@@ -1,13 +1,17 @@
 from math import floor
 
-system_data = {
-    "RSE": {"frame_rate": (16777216 / 280896), "offset_ms": 0},   
-    "GBA": {"frame_rate": (16777216 / 280896), "offset_ms": -260},
-    "GBP": {"frame_rate": (16777216 / 280896), "offset_ms": 200},
-    "NDS": {"frame_rate": (16756991 / 280896), "offset_ms": 788},
-    "3DS": {"frame_rate": (16756991 / 280896), "offset_ms": 1558},
+SYSTEM_TIMING_DATA = {
+    "RSE": {"frame_rate": 16777216 / 280896, "offset_ms": 0},
+    "GBA": {"frame_rate": 16777216 / 280896, "offset_ms": -260},
+    "GBP": {"frame_rate": 16777216 / 280896, "offset_ms": 200},
+    "NDS": {"frame_rate": 16756991 / 280896, "offset_ms": 788},
+    "3DS": {"frame_rate": 16756991 / 280896, "offset_ms": 1558},
 }
 
-def frame_to_ms(frame, system):
-        return floor((frame) / (system_data[system]["frame_rate"]) * 1000) + system_data[system]["offset_ms"]
 
+def frame_to_ms(frame: int, system: str = "RSE"):
+    """Convert frame to milliseconds based on the selected system"""
+    return (
+        floor(frame / SYSTEM_TIMING_DATA[system]["frame_rate"] * 1000)
+        + SYSTEM_TIMING_DATA[system]["offset_ms"]
+    )
